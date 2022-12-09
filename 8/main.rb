b56f399c111a5e9a942cde7152563737f0dba2e5
Geoffrey Donaldson <geoffreyd@gmail.com>
2022-12-09 14:11:28 +1100

    Day 8 - Part 2

--- 8/main.rb
+++ 8/main.rb
@@ -33,3 +33,46 @@ pp visibility
 visible_count = visibility.flatten.count(1)
 
 pp visible_count
+
+## Part 2
+
+def scenic_score(height, heights)
+  return 0 if heights.empty?
+
+  lower = heights.index { |h| h >= height }
+  return heights.length if lower.nil?
+
+  lower + 1
+end
+
+# For each number in the grid count the number of positivly incrementing numbers in each direction
+scenic_scores = grid.map.with_index do |row, y|
+  row.map.with_index do |number, x|
+    scores = [
+      # up
+      scenic_score(number, (0...y).map { |i| grid[i][x] }.reverse),
+      # left
+      scenic_score(number, (0...x).map { |i| grid[y][i] }.reverse),
+      # down
+      scenic_score(number, (y + 1...height).map { |i| grid[i][x] }),
+      # right
+      scenic_score(number, (x + 1...width).map { |i| grid[y][i] })
+    ]
+
+    score = scores.inject(:*)
+
+    if score.positive?
+      pp "x: #{x}, y: #{y}, num: #{number}, score: #{score}"
+      pp scores
+    end
+
+    score
+  end
+end
+
+# print a table of the scores
+scenic_scores.each do |row|
+  puts row.map { |s| s.to_s.rjust(4, ' ') }.join(' ')
+end
+
+pp scenic_scores.flatten.max
