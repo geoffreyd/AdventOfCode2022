319072d8b59a94ffc4e91154bd169335a77cdc6a
Geoffrey Donaldson <geoffreyd@gmail.com>
2022-12-10 16:22:57 +1000

    clean up

--- 6/main.rb
+++ 6/main.rb
@@ -6,7 +6,7 @@ line = IO.readlines(path, chomp: true).first
 part2 = false
 offset = part2 ? 14 : 4
 
-marker, idx = line.chars.each_cons(offset).with_index.find do |chars, _|
+_, idx = line.chars.each_cons(offset).with_index.find do |chars, _|
   chars.uniq.size == offset
 end
 
