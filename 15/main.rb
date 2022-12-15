783846551d77ac94c1595cc9d1ab15c3247256fe
Geoffrey Donaldson <geoffreyd@gmail.com>
2022-12-15 19:09:21 +1000

    Day 15 - Part 2 - refactored
    Now down to 5-6 seconds (from ~60)

--- 15/main.rb
+++ 15/main.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require 'set'
+
 require 'debug'
 
 path = './input.txt'
@@ -8,8 +8,8 @@ lines = IO.readlines(path, chomp: true)
 @grid = {}
 
 lower_bounds = 0
-upper_bounds = 4000000
-scanned_points = []
+upper_bounds = 4_000_000
+# scanned_points = []
 
 lines.each do |line|
   # extract the data from lines that look like this:
@@ -19,13 +19,12 @@ lines.each do |line|
   # puts "Found details: #{s_x}, #{s_y}, #{b_x}, #{b_y}, #{manhattan_distance}"
   @grid[[s_x, s_y]] = manhattan_distance
   # @grid[[b_x, b_y]] = true
-
 end
 
-points = scanned_points - @grid.keys
+# points = scanned_points - @grid.keys
 
 # puts "Points: #{points.uniq.sort}"
-puts "Answer: #{points.uniq.count}"
+# puts "Answer: #{points.uniq.count}"
 
 scanner_points = @grid.keys
 
@@ -41,55 +40,39 @@ def border_points(point, distance)
   boundary_coordinates.uniq
 end
 
-possible_points = Set.new
 found = nil
 # checked = Set.new
 
 scanner_points.each_index do |i|
   break if found
+
   # for each point around the border of this scanners manhattan distance
-  # check if there is another scanner in distance, this is not our point
-  # if there is no other scanner in distance, this is our point
+  # If there is another scanner in distance, move on.
+  # If there is no other scanner in distance, this is our point.
   s_x, s_y = scanner_points[i]
   manhattan_distance = @grid[[s_x, s_y]]
   puts "Checking Scanner #{i} with distance #{manhattan_distance}"
 
-  scanner_points[i + 1..-1].each_with_index do |scanner_point, j|
-    break if found
-    print "Looking at scanner: #{j} ... "
-
+  possible_overlaps = scanner_points.reject do |scanner_point|
     other_scanner_distance = @grid[scanner_point]
     distance_between_scanners = (scanner_point[0] - s_x).abs + (scanner_point[1] - s_y).abs
-    if distance_between_scanners > manhattan_distance + other_scanner_distance + 1
-      puts 'Skipping, distance between scanners is greater than sum of distances'
-      next
-    end
-    if manhattan_distance > distance_between_scanners + other_scanner_distance - 1
-      puts 'Skipping, other scanner is within bounds of this scanner'
-      next
-    end
-    if distance_between_scanners + manhattan_distance < other_scanner_distance + 1
-      puts 'Skipping, this scanner is within bounds of other scanner'
-      next
-    end
-    puts 'There is overlap, Checking points'
 
-    border_points([s_x, s_y], manhattan_distance).each do |point|
-      x, y = point
-      next if x < lower_bounds || x > upper_bounds || y < lower_bounds || y > upper_bounds
+    distance_between_scanners > manhattan_distance + other_scanner_distance + 1 ||
+      manhattan_distance > distance_between_scanners + other_scanner_distance ||
+      distance_between_scanners + manhattan_distance < other_scanner_distance
+  end
 
-      # checked << point
-      # puts "Checking border point: #{point}"
-      if scanner_points.any? { |scanner_point|
-        (scanner_point[0] - x).abs + (scanner_point[1] - y).abs <= @grid[scanner_point]
-      }
-        # puts "Found scanner in distance"
-      else
-        puts "Found point: #{point}"
-        found = point
-        break
-      end
+  border_points([s_x, s_y], manhattan_distance).each do |point|
+    x, y = point
+    next if x < lower_bounds || x > upper_bounds || y < lower_bounds || y > upper_bounds
+
+    # puts "Checking border point: #{point}"
+    next if possible_overlaps.any? do |scanner_point|
+      (scanner_point[0] - x).abs + (scanner_point[1] - y).abs <= @grid[scanner_point]
     end
+
+    found = point
+    break
   end
 end
 
